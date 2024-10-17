d632d58781465ff03be2da2cc0d469213a28bf5a
Devamani11D <duddekuntadevamani@gmail.com>
2024-10-17 17:49:44 +0530

    Implemented Public consortium

--- contracts/NexTrade.sol
+++ contracts/NexTrade.sol
@@ -1,7 +1,11 @@
 // contracts/NexTrade.sol
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract NexTrade {
+contract NexTrade is AccessControl {
+    bytes32 public constant BROKER_ROLE = keccak256("BROKER_ROLE");
+    bytes32 public constant TRADER_ROLE = keccak256("TRADER_ROLE");
+
     struct Stock {
         string name;
         uint256 price;
@@ -10,13 +14,22 @@ contract NexTrade {
 
     mapping(string => Stock) public stocks;
 
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(BROKER_ROLE, msg.sender); // Assigning Broker Role to the contract deployer initially
+    }
+
     function addStock(string memory _name, uint256 _price) public {
         require(stocks[_name].price == 0, "Stock already exists");
+        require(hasRole(BROKER_ROLE, msg.sender), "Only brokers can add stocks");
         stocks[_name] = Stock(_name, _price, msg.sender);
     }
 
     function buyStock(string memory _name) public payable {
         Stock storage stock = stocks[_name];
+        require(hasRole(TRADER_ROLE, msg.sender), "Only traders can buy stocks");
+        require(stocks[name].price > 0, "Stock does not exist");
+        require(msg.value >= stocks[name].price, "Not enough Ether sent");
         require(msg.value == stock.price, "Incorrect amount");
         require(stock.owner != address(0), "Stock does not exist");
 
@@ -29,3 +42,5 @@ contract NexTrade {
         return stocks[_name].owner;
     }
 }
+
+
